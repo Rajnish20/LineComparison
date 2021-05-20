9478b6f36b4661e9ef8a67d412e49c6eff2365f4
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 21:29:50 +0530

    model a line and calculated its length

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,8 +1,25 @@
 package com.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparison");
+		
+		System.out.println("Line 1");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter x1");
+		int l1x1 = scanner.nextInt();
+		System.out.println("Enter y1");
+		int l1y1 = scanner.nextInt();
+		System.out.println("Enter x2");
+		int l1x2 = scanner.nextInt();
+		System.out.println("Enter y2");
+	    int l1y2 = scanner.nextInt();
+		
+	    Double length1 = Math.sqrt(Math.pow((l1x2 - l1x1), 2) + Math.pow((l1y2 - l1y1), 2));
+		
+		
 	}
 
 }
