88690af7f56cb203b8ce4f0dea85634495f966a5
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 21:43:56 +0530

    Refactor to object oriented

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,48 +1,61 @@
 package com.linecomparison;
 
-import java.util.Scanner;
 
 public class LineComparison {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Line Comparison");
+		System.out.println("Wecome To Line Comparison Problem");
 
-		System.out.println("Line 1");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter x1");
-		int l1x1 = scanner.nextInt();
-		System.out.println("Enter y1");
-		int l1y1 = scanner.nextInt();
-		System.out.println("Enter x2");
-		int l1x2 = scanner.nextInt();
-		System.out.println("Enter y2");
-		int l1y2 = scanner.nextInt();
+		Line Line1 = new Line(4, 7, 10, 12);
+		Line Line2 = new Line(5, 9, 11, 15);
 
-		Double length1 = Math.sqrt(Math.pow((l1x2 - l1x1), 2) + Math.pow((l1y2 - l1y1), 2));
+		Double length1 = Length.calculateLength(Line1);
+		Double length2 = Length.calculateLength(Line2);
 
-		System.out.println("Line 2");
-		System.out.println("Enter x1");
-		int l2x1 = scanner.nextInt();
-		System.out.println("Enter y1");
-		int l2y1 = scanner.nextInt();
-		System.out.println("Enter x2");
-		int l2x2 = scanner.nextInt();
-		System.out.println("Enter y2");
-		int l2y2 = scanner.nextInt();
+		System.out.println(Equality.equalsTo(length1, length2));
 
-		Double length2 = Math.sqrt(Math.pow((l2x2 - l2x1), 2) + Math.pow((l2y2 - l2y1), 2));
+		System.out.println(Comparison.compareTo(length1, length2));
+	}
+}
 
-		boolean isEqual = length1.equals(length2);
-		if (isEqual == true)
-			System.out.println("Lines are Equal");
-		else
-			System.out.println("Lines are not Equal");
-		
-		int result = length1.compareTo(length2);
-		if(result < 0)
-			System.out.println("Line 2 is greater than Line 1");
-		else
-			System.out.println("Line 1 is greater than Line 2");
+class Line {
+	int x1;
+	int y1;
+	int x2;
+	int y2;
 
+	public Line(int x1, int y1, int x2, int y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
 	}
 
+}
+
+class Length {
+	public static Double calculateLength(Line line) {
+		return Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2));
+	}
+}
+
+class Equality {
+	public static String equalsTo(Double L1, Double L2) {
+		boolean result = L1.equals(L2);
+		if (result == true)
+			return "Lines are Equal";
+		else
+			return "Lines are not Equal";
+	}
+}
+
+class Comparison {
+	public static String compareTo(Double L1, Double L2) {
+		int result = L1.compareTo(L2);
+		if (result < 0)
+			return "Line 2 is greater than Line 1";
+		else if (result > 0)
+			return "Line 1 is greater than Line 2";
+		else
+			return "Lines are Equal";
+	}
 }
