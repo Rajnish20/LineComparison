e955202301f68ed9a7483654b987351388c96e54
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 21:34:29 +0530

    Added Equality logic

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparison");
-		
+
 		System.out.println("Line 1");
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter x1");
@@ -15,11 +15,28 @@ public class LineComparison {
 		System.out.println("Enter x2");
 		int l1x2 = scanner.nextInt();
 		System.out.println("Enter y2");
-	    int l1y2 = scanner.nextInt();
-		
-	    Double length1 = Math.sqrt(Math.pow((l1x2 - l1x1), 2) + Math.pow((l1y2 - l1y1), 2));
-		
-		
+		int l1y2 = scanner.nextInt();
+
+		Double length1 = Math.sqrt(Math.pow((l1x2 - l1x1), 2) + Math.pow((l1y2 - l1y1), 2));
+
+		System.out.println("Line 2");
+		System.out.println("Enter x1");
+		int l2x1 = scanner.nextInt();
+		System.out.println("Enter y1");
+		int l2y1 = scanner.nextInt();
+		System.out.println("Enter x2");
+		int l2x2 = scanner.nextInt();
+		System.out.println("Enter y2");
+		int l2y2 = scanner.nextInt();
+
+		Double length2 = Math.sqrt(Math.pow((l2x2 - l2x1), 2) + Math.pow((l2y2 - l2y1), 2));
+
+		boolean isEqual = length1.equals(length2);
+		if (isEqual == true)
+			System.out.println("Lines are Equal");
+		else
+			System.out.println("Lines are not Equal");
+
 	}
 
 }
